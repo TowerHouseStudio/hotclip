e7bf8ebf2527e618eff37644846c63882994ed44
elsolenllamas <elsolenllamas@gmail.com>
2014-06-06 21:25:44 -0300

    switch tab oculta loginbox

---
+++ libs/scripts-ui.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+  $( ".arrow-tab" ).click(function() {
+  $( ".frame-boxes" ).animate({
+     width: "toggle"
+  }, 100, function() {
+    $( ".frame-boxes-hide" ).show();
+    
+  });
+});
+
+    $( ".arrow-tab-hide" ).click(function() {
+      $( ".frame-boxes-hide" ).hide( 100, function() {
+    $( ".frame-boxes" ).animate({
+     width: "toggle"
+  }, 100, function() {
+      
+  });
+  });
+  
+});
+
+ });
