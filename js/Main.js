93f91356e5562d82e6d46b2155d403673a92ac84
elsolenllamas <elsolenllamas@gmail.com>
2014-06-06 22:52:29 -0300

    tag click js

--- js/Main.js
+++ js/Main.js
@@ -2,6 +2,7 @@ function Main(){
     var aVideoObject = document.getElementById('video_content');
     var aVideoOverlay = $.find("#video_overlay");
     $(aVideoOverlay).on("click", function(){
+    	$(".tag-click-content").show();
         aVideoObject.playVideo();
     });
 
